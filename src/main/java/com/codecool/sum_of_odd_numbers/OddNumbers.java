eeb5495fbd32ee486352dfa51e957a5f25234e60
huszarpp <huszarpp@gmail.com>
2021-03-24 17:49:04 +0100

    Reduce accesses in OddNumbers

--- src/main/java/com/codecool/sum_of_odd_numbers/OddNumbers.java
+++ src/main/java/com/codecool/sum_of_odd_numbers/OddNumbers.java
@@ -2,9 +2,9 @@ package com.codecool.sum_of_odd_numbers;
 
 import java.util.List;
 
-public class OddNumbers {
+class OddNumbers {
 
-    public static Integer sum(List<Integer> numbers) {
+    static Integer sum(List<Integer> numbers) {
 
         int sumOfOddNumbers = 0;
 
